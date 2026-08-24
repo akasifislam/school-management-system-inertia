778d3d5574e4b54ef2bafe488d2831f5e8ecbb90
akasifislam <aseasifislam@gmail.com>
2026-08-24 23:36:45 +0600

    school

--- resources/views/admin/pages/results_form.blade.php
+++ resources/views/admin/pages/results_form.blade.php
@@ -21,6 +21,10 @@
                     <input type="text" name="title" class="form-control"
                         value="{{ old('title', $result->title ?? '') }}" required>
                 </div>
+                <div>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam quia pariatur error minus soluta sunt
+                    expedita esse? Nemo earum corporis voluptas itaque saepe nostrum mollitia iste porro, unde autem quasi.
+                </div>
                 <div class="form-row">
                     <div class="form-group">
                         <label>পরীক্ষার ধরন *</label>
@@ -34,7 +38,6 @@
                             @endforeach
                         </select>
                     </div>
-
                     <div class="form-group">
                         <label>বছর *</label>
                         <input type="number" name="year" class="form-control"
