76b4903e93e650bdf917c19f0b2abcd10d8a514e
akasifislam <aseasifislam@gmail.com>
2026-09-05 23:54:20 +0600

    school

--- app/Models/NewsItem.php
+++ app/Models/NewsItem.php
@@ -7,6 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class NewsItem extends Model
 {
     protected $fillable = ['title', 'link', 'is_active'];
-    
     protected $casts    = ['is_active' => 'boolean'];
 }
